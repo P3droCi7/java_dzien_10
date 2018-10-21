79e93fa008e29d0089b87446973f5fad73d2b74f
Michal Skrzypczak <michalskrzypczak91@gmail.com>
2018-10-21 10:44:29 +0200

    V1

--- src/NWD.java
+++ src/NWD.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class NWD {
+
+
+
     public static void main(String[] args) {
+        System.out.println(countNWD());
 
     }
+
+    public static double countNWD() {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (b == 0) {
+            return a;
+        }
+
+        double wynik = a % b;
+        return wynik;
+    }
+
 }
