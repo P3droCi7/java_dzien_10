56c5a708755ee222b464e05d982710d8f98e7c90
Michal Skrzypczak <michalskrzypczak91@gmail.com>
2018-10-21 11:33:15 +0200

    V@ ostateczna

--- src/NWW.java
+++ src/NWW.java
@@ -1,14 +1,23 @@
 import java.util.Scanner;
 
-public class NWD {
+public class NWW {
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
 
-        System.out.println(countNWD(a, b));
+        System.out.println(countNWW(a, b));
     }
+
+
+    public static double countNWW(int a, int b){
+    double wynik = (a*b)/(countNWD(a,b));
+    return wynik;
+    }
+
+
     public static double countNWD(int a, int b) {
 
         if (b == 0) {
@@ -18,3 +27,4 @@ public class NWD {
     }
 
 }
+
